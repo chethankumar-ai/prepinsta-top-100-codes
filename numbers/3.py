920bc1f4fb90b94864368ecc667db59af374307e
chethankumar-ai <chethankumarchethan25@gmail.com>
2025-05-08 19:54:49 +0530

    check given number is Even or Odd using bit wise operator

--- numbers/3.py
+++ numbers/3.py
@@ -1,4 +1,4 @@
-# This Method uses bitwise operators to check if a given number is Even or Odd.
+# 3.This Method uses bitwise operators to check if a given number is Even or Odd.
 def isEven(num):
     return not num & 1
 if __name__ =="__main__":
