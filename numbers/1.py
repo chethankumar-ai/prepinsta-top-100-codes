b6e31baf97c746a93d79c97bf5fd464ad270e394
chethankumar-ai <chethankumarchethan25@gmail.com>
2025-05-08 19:23:54 +0530

    Initial commit - Top 100 PrepInsta Codes

--- numbers/1.py
+++ numbers/1.py
@@ -1,2 +1,9 @@
 # Python Program to check if a Number Is Positive Or Negative
 num  = int(input("enter the number "))
+if num > 0:
+    print("number is positive")
+elif num <= 0:
+    print("number is negative")
+else:
+    print("number is zero")
+    
